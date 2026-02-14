4a561225d1950001cdef1039888d082c7cf50a77
ZainUlBasit <zainulbasit486@gmail.com>
2026-02-14 11:42:32 +0500

    Added new allowed origin for local development in API configuration.

--- api/index.js
+++ api/index.js
@@ -19,6 +19,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:3001",
   "http://localhost:5173",
+  "http://localhost:5171",
   "http://localhost:5174",
   "http://localhost:5175",
   "http://localhost:5176",
